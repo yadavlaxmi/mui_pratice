1dabf43451defff61e5ab62082132d17a59eeaf7
laxmi <laxmiyadav21@navgurukul.org>
2023-10-06 15:22:38 +0530

    first commit of mui

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import {Button} from "@material-ui/core"
+
+function App() {
+  return (
+    <>
+    <Button variant="contained" color="primary">my name laxmi</Button>
+    </>
+  );
+}
+
+export default App;
