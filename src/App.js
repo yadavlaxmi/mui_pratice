260bf4a7a926cedc080de67ddd1d2e59aa141882
laxmi <laxmiyadav21@navgurukul.org>
2023-10-06 15:47:02 +0530

    mui_button

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
   return (
     <>
     <Button variant="contained" color="primary">my name laxmi</Button>
+    <Button variant="contained" color="secondary">my name </Button>
+    <Button variant="contained" color="default">my </Button>
+
     </>
   );
 }
