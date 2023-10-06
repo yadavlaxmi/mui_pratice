3983a0f32f4ffe24c2993cbe444fa9015a62a524
laxmi <laxmiyadav21@navgurukul.org>
2023-10-06 19:48:34 +0530

    use the property size and use the icon of delete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import React,{useState} from "react";
-import './App.css';
-import {Button} from "@material-ui/core"
+import React, { useState } from "react";
+import "./App.css";
+import { Button } from "@material-ui/core";
+import Delete  from "@material-ui/icons/Delete";
 
 function App() {
-    const[color,setColor]=useState("primary")
-  
-function customeMe(){
-    setColor("secondary")
-}
+  const [color, setColor] = useState("primary");
+  const [disableBtn, setDisableBtn] = useState(false);
+
+  function customMe() {
+    //setColor("secondary"); // Change the color to "secondary" when clicked
+    setDisableBtn(true);
+  }
+
   return (
     <>
-    <Button 
-    variant="outlined" 
-    color={color} 
-    onClick={()=>{customeMe()}}>
-    click me</Button>
-    
-
+      <Button
+        variant="outlined"
+        disabled={disableBtn}
+        color={color}
+        size="small"
+        onClick={() => {
+          customMe();
+        }}
+      endIcon={<Delete />}
+      >
+        click me
+      </Button>
     </>
   );
-
 }
+
 
 export default App;
