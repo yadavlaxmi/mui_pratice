c9dc953107b5437c01416be1e846574924df1a4e
laxmi <laxmiyadav21@navgurukul.org>
2023-10-06 16:03:47 +0530

    button by click

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 import {Button} from "@material-ui/core"
 
 function App() {
   return (
     <>
-    <Button variant="contained" color="primary">my name laxmi</Button>
-    <Button variant="contained" color="secondary">my name </Button>
+    <Button variant="contained" color="primary" onClick={()=>alert("laxmi")}>my name laxmi</Button>
+    <Button variant="outlined" color="secondary">my name </Button>
     <Button variant="contained" color="default">my </Button>
 
     </>
