f5608281c1d534a467d6b5c2e9211081cfc94b88
laxmi <laxmiyadav21@navgurukul.org>
2023-10-07 13:54:06 +0530

    Button group ,orientation and properties

--- src/App.js
+++ src/App.js
@@ -1,34 +1,17 @@
 import React, { useState } from "react";
 import "./App.css";
-import { Button } from "@material-ui/core";
-import Delete  from "@material-ui/icons/Delete";
-
-function App() {
-  const [color, setColor] = useState("primary");
-  const [disableBtn, setDisableBtn] = useState(false);
-
-  function customMe() {
-    //setColor("secondary"); // Change the color to "secondary" when clicked
-    setDisableBtn(true);
-  }
-
+import { ButtonGroup,Button } from "@material-ui/core";
+function App(){
   return (
     <>
-      <Button
-        variant="outlined"
-        disabled={disableBtn}
-        color={color}
-        size="small"
-        onClick={() => {
-          customMe();
-        }}
-      endIcon={<Delete />}
-      >
-        click me
-      </Button>
+      <ButtonGroup orientation="vertical" variant="outlined" color="primary">
+      <Button>one</Button>
+      <Button>two</Button>
+      <Button>three</Button>
+      </ButtonGroup>
     </>
   );
-}
+  }
 
 
 export default App;
