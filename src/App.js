c9a676065caa90c652e561b42410718da09078e3
laxmi <laxmiyadav21@navgurukul.org>
2023-10-07 20:07:28 +0530

    slider range

--- src/App.js
+++ src/App.js
@@ -2,38 +2,22 @@ import React, { useState } from "react";
 import "./App.css";
 import {Slider} from "@material-ui/core"
 function App(){
-          const mark=[{
-            value:0,
-            label:"start"
-          },
-          {
-            value:100,
-            label:"middle"
-
-          },
-          {
-
-            value:30,
-            label:"at 30"
-          },
-          {
-
-            value:200,
-            label:"end"
-          }
-        ]
-            
+    const[val,setVal]=useState([40,100])
+     const updateVal=(e,item)=>{
+      setVal(item)
+     }
   return (
     <div className="App">
     <h1> Slider</h1>
-    <div style={{width:300,margin:40}}>
-      <Slider color="primary" 
-      defaultValue={100} 
-      max={200} 
-      step={10}
-       marks={mark}
-      />
-      </div>
+    <div style={{width:300,margin:30}}> 
+    <Slider value={val}
+    max={200}
+    step={10}
+    onChange={updateVal}
+    valueLabelDisplay="auto"/>
+    </div>
+    
+  
     
     </div>
   );
