b02ce7c05a199d6eb90408763cc1391720785bdf
laxmi <laxmiyadav21@navgurukul.org>
2023-10-07 15:34:49 +0530

    interminate property in checkbox ,onchange event on checkbox,get value of multiple checkbox and custom icon in checkbox

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React, { useState } from "react";
 import "./App.css";
-import Checkbox from "@material-ui/core/Checkbox"
+import{Favorite} from "@material-ui/icons";
+import{FavoriteBorder} from "@material-ui/icons";
+
+import Checkbox from "@material-ui/core/Checkbox";
 function App(){
+   const[name, setName]=useState([])
   function getValue(e)
   {
-    console.warn(e.target.value)
+    let data=name;
+    data.push(e.target.value)
+    console.warn(data)
   }
 
   return (
     <>
       <h1> my checkbox</h1>
       <Checkbox color="primary" value="laxmi"onChange={(e)=>{getValue(e)}}/>
+      <Checkbox color="secondary" value="Mansa"onChange={(e)=>{getValue(e)}}/>
+      <Checkbox color="default" value="guddi"onChange={(e)=>{getValue(e)}}/>
+      <Checkbox color="primary" value="xyz" indeterminate onChange={(e)=>{getValue(e)}}/>
+      <Checkbox color="secondary"
+       value="xyz"  onChange={(e)=>{getValue(e)}} 
+       icon={<FavoriteBorder/>}
+       checkedIcon={<Favorite/>}/>
+
     </>
   );
   }
