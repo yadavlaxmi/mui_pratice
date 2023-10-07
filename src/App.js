b790016e0d914447fbe09c488eebc0cb26c2fede
laxmi <laxmiyadav21@navgurukul.org>
2023-10-07 18:15:07 +0530

    usestate with radio button

--- src/App.js
+++ src/App.js
@@ -2,21 +2,35 @@ import React, { useState } from "react";
 import "./App.css";
 import{Radio} from "@material-ui/core";
 function App(){
-    const testfunction=()=>{
-      alert("gender")
+  const[gender,setGender]=useState("male")
+    const testfunction=(e)=>{
+      console.warn(e.target.value)
+      setGender(e.target.value)
 
     }
   return (
     <>
       <h1> React Material UI Radio Button </h1>
       <div>
-        <Radio color="primary" value="Male"
+        <Radio color="primary" value="male"
+        checked={gender==="male"}
         onChange={(testfunction)} />
         <span> Male </span>
       </div>
       <div>
-        <Radio color="secondary" value="Female"/>
+        <Radio color="secondary" 
+        value="female" checked={gender==="female"}
+
+        onChange={(testfunction)}/>
         <span> Female </span>
+
+      </div>
+      <div>
+        <Radio color="default" 
+        value="other" checked={gender==="other"}
+
+        onChange={(testfunction)}/>
+        <span> other </span>
 
       </div>
     </>
