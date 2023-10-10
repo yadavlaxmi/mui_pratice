e7cbfd3e86e9ca910b82577e1280104da9ceaaf7
laxmi <laxmiyadav21@navgurukul.org>
2023-10-10 15:38:28 +0530

    Hidden layout for single layout

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React, { useState } from "react";
 import "./App.css";
-import {Grid} from "@material-ui/core"
+import {Grid,Hidden} from "@material-ui/core"
 function App(){
 
   
     
   return (
-   <Grid item xs={12} container spacing={1}>
-    <Grid item lg={3} sm={6} xs={12}><h1 style={{background:"green"}}>block1</h1></Grid>
-    <Grid item lg={3} sm={6} xs={12}><h1 style={{background:"green"}}>block2</h1></Grid>
-    <Grid item lg={3} sm={6} xs={12}><h1 style={{background:"green"}}>block3</h1></Grid>
-    <Grid item lg={3} sm={6} xs={12}><h1 style={{background:"green"}}>block4</h1></Grid>
-
+   <Grid  container spacing={2} xs={12}>
+    <Grid item lg={3}  sm={6} xs={12}><h1 style={{backgroundColor:"green"}}>BLOCK 1</h1></Grid>
+    <Grid item lg={3} sm={6} xs={12}><h1 style={{backgroundColor:"green"}}>BLOCK 2</h1></Grid>
+    <Hidden only="sm"><h1 style={{backgroundColor:"green",flex:1}}>BLOCK3</h1></Hidden>
+    <Grid item lg={3} sm={6} xs={12}><h1 style={{backgroundColor:"green"}}>BLOCK4</h1></Grid>
 
    </Grid>
   );
-  }
+  } 
 
 
 export default App;
