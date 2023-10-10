717ddc88e4d474daa7fe6034535c1419e9520aea
laxmi <laxmiyadav21@navgurukul.org>
2023-10-10 16:37:52 +0530

    for multiple hidden the layout

--- src/App.js
+++ src/App.js
@@ -1,53 +1,20 @@
 import React, { useState } from "react";
 import "./App.css";
-import { Tab, Tabs, AppBar } from "@material-ui/core";
+import {Grid,Hidden} from "@material-ui/core"
+function App(){
 
-export default function App() {
-  const [value, setValue] = React.useState(0);
-
-  const handleTabs = (e, val) => {
-    console.warn(val);
-    setValue(val); // You forgot to set the value here
-  };
-
+  
+    
   return (
-    <div>
-      <h1>React Material UI</h1>
-      <AppBar position="static">
-        <Tabs value={value} onChange={handleTabs}>
-          <Tab label="Item 1" />
-          <Tab label="Item 2" />
-          <Tab label="Item 3" />
-          <Tab label="Item 4" />
-        </Tabs>
-      </AppBar>
-      <TabPanel value={value} index={0}>
-        Item one detail
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        Item two detail
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        Item three detail
-      </TabPanel>
-      <TabPanel value={value} index={3}>
-        Item four detail
-      </TabPanel>
-    </div>
-  );
-}
+   <Grid  container spacing={2} xs={12}>
+    <Grid item lg={3}  sm={6} xs={12}><h1 style={{backgroundColor:"green"}}>BLOCK 1</h1></Grid>
+    <Grid item lg={3} sm={6} xs={12}><h1 style={{backgroundColor:"green"}}>BLOCK 2</h1></Grid>
+    <Hidden only={["sm","lg"]}><h1 style={{backgroundColor:"green",flex:1}}>BLOCK3</h1></Hidden>
+    <Grid item lg={3} sm={6} xs={12}><h1 style={{backgroundColor:"green"}}>BLOCK4</h1></Grid>
 
-function TabPanel(props) {
-  const { children, value, index } = props;
-
-  return (
-    <div>
-      {value === index && (
-        <div>
-          <h1> {children}</h1>
-          
-        </div>
-      )}
-    </div>
+   </Grid>
   );
-}
+  } 
+
+
+export default App;
