54cb018f0f017dc1ccbc97582de5c0f870cf1778
laxmi <laxmiyadav21@navgurukul.org>
2023-10-10 13:48:28 +0530

    grid system for responsive layout

--- src/App.js
+++ src/App.js
@@ -1,34 +1,19 @@
 import React, { useState } from "react";
 import "./App.css";
-import {Container} from "@material-ui/core"
+import {Grid} from "@material-ui/core"
 function App(){
 
   
     
   return (
-    <div>
-    <Container  style={{backgroundColor:"skyblue"}} maxWidth="lg">  
-    <h1>  React material UI container layout</h1>
-    </Container>
-    <Container  style={{backgroundColor:"skyblue"}} maxWidth="md">  
-    <h1>  React material UI container layout</h1>
-    </Container>
-    <Container  style={{backgroundColor:"skyblue"}} maxWidth="sm">  
-    <h1>  React material UI container layout</h1>
-    </Container>
-    <Container  style={{backgroundColor:"skyblue"}} maxWidth="xs">  
-    <h1>  React material UI container layout</h1>
-    </Container>
-    <Container fixed style={{backgroundColor:"skyblue"}}>  
-    <h1>  React material UI container layout</h1>
-    </Container>
-   
-    
-    
-   
-  
-    
-    </div>
+   <Grid item xs={12} container spacing={1}>
+    <Grid item lg={3} sm={6} xs={12}><h1 style={{background:"green"}}>block1</h1></Grid>
+    <Grid item lg={3} sm={6} xs={12}><h1 style={{background:"green"}}>block2</h1></Grid>
+    <Grid item lg={3} sm={6} xs={12}><h1 style={{background:"green"}}>block3</h1></Grid>
+    <Grid item lg={3} sm={6} xs={12}><h1 style={{background:"green"}}>block4</h1></Grid>
+
+
+   </Grid>
   );
   }
 
